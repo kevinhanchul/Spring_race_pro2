6e6839e1b4480a985bab05cca293afc83ff64111
한철 <chul.han@biplatform.co.kr>
2024-09-25 14:54:19 +0900

    인에서 커밋합니다.

--- src/main/java/com/example/demo/service/RaceInfoService.java
+++ src/main/java/com/example/demo/service/RaceInfoService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.mapper.RaceInfoMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -16,9 +15,6 @@ public class RaceInfoService {
     }
 
     public Map<String, Object> getRaceInfo(Long id) {
-    	Map<String, Object> race_result = raceInfoMapper.selRaceInfo(id);
-        //Map<String, Object> resultMap = new HashMap<>();
-        //resultMap.put("race_result", race_result);
-        return race_result;
+        return raceInfoMapper.selRaceInfo(id);
     }
 }
